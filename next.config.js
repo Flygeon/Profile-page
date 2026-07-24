a2e11c4dce01a7f0042af16c32a172c1db3d1611
Flygeon <v2972632024@163.com>
2026-07-24 19:38:53 +0800

    静态导出

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // 添加这一行，启用静态导出
+  
   images: {
+    unoptimized: true,  // 静态导出必须关闭内置图片优化
     remotePatterns: [
       {
         protocol: 'https',
