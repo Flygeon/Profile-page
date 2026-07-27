24f540f9ea301acfcc840642be8887e4357d2527
Flygeon <Flygeon@users.noreply.github.com>
2026-07-27 15:43:21 +0000

    Deploy from Astro update 3d10d7b27dc6d736cfc831b56a77258daaaccd33 3d10d7b27dc6d736cfc831b56a77258daaaccd33

---
+++ _next/static/chunks/app/layout-48e8062df988ada4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{405:function(n,e,u){Promise.resolve().then(u.t.bind(u,8035,23))},8035:function(){}},function(n){n.O(0,[770,72,352,68,541,744],function(){return n(n.s=405)}),_N_E=n.O()}]);
